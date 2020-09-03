5d85d7197c582f81c5fac08aaa8f1ce4991b70f8
PhilipVigus <philipvigus@yahoo.co.uk>
2020-09-03 14:15:52 +0100

    Implement quicksort for longer arrays

--- src/quickSort.js
+++ src/quickSort.js
@@ -1,5 +1,19 @@
 const quickSort = (list) => {
-  return list;
+  if (list.length < 2) {
+    return list;
+  } else {
+    const pivot = list[0];
+
+    const elementsLower = list.slice(1).filter((element) => {
+      return element <= pivot;
+    });
+
+    const elementsHigher = list.slice(1).filter((element) => {
+      return element > pivot;
+    });
+
+    return [...quickSort(elementsLower), pivot, ...quickSort(elementsHigher)];
+  }
 };
 
 export default quickSort;
--- src/tests/quickSort.test.js
+++ src/tests/quickSort.test.js
@@ -8,4 +8,8 @@ describe("quickSort", () => {
   it("sorts an array with a single element", () => {
     expect(quickSort([1])).toEqual([1]);
   });
+
+  it("sorts an array with multiple elements", () => {
+    expect(quickSort([1, 3, 2, 5])).toEqual([1, 2, 3, 5]);
+  });
 });
